7ee5d1c0651144aceecf1548c2207dc00ea860fe
saro1993 <116103507+saro1993@users.noreply.github.com>
2024-08-14 15:07:36 +0200

    make it better

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,12 +1,14 @@
+
 export function Login() {
     async function handleStartLogin() {
       const { authorization_endpoint } = await fetchJson(
         "https://accounts.google.com/.well-known/openid-configuration"
       );
+      const REACT_APP_CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
       const query = new URLSearchParams({
         response_type: "token",
         scope: "openid profile email",
-        client_id: "your-client-id",
+        REACT_APP_CLIENT_ID: "your-client-id",
         redirect_uri: window.location.origin + "/callback",
       });
       window.location.href = authorization_endpoint + "?" + query;
